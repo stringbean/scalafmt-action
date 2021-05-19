b178025aff574597ab3ba1eb81e446b1a65e1086
Michael Stringer <stringbean@users.noreply.github.com>
2021-05-19 13:00:31 +0100

    Run coverage on build

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageDirectory: 'target/coverage',
+  coverageReporters: ['text-summary', 'lcov', 'cobertura'],
+  collectCoverageFrom: ['./src/**/*.ts'],
 };
